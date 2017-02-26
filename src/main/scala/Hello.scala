28773858d8e711004f038504e361c5995fd74fae
Huajian Mao <huajianmao@gmail.com>
2017-02-26 20:00:40 +0800

    Remove not started chapter directories.

--- src/main/scala/Hello.scala
+++
@@ -1,9 +0,0 @@
-
-object Hello extends App {
-  val p = Person("Alvin Alexander")
-  // scalastyle:off println
-  println("Hello from " + p.name)
-  // scalastyle:on println
-}
-
-case class Person(var name: String)
