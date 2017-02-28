850ed3eb053165ac376b94690dc0c807cc8d8abc
Huajian Mao <huajianmao@gmail.com>
2017-02-28 17:21:37 +0800

    Add answers and tests for Exercise 3.16 ~ 3.23.

--- src/main/scala/fpinscala/exercises/ch03datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/ch03datastructures/List.scala
@@ -174,4 +174,126 @@ object List {
   def concat[A](listsList: List[List[A]]): List[A] = {
     foldLeft(listsList, Nil: List[A])((merged, next) => List.append(merged, next))
   }
+
+  /**
+   * Exercise 3.16
+   * Write a function that transforms a list of integers by adding 1 to each element.
+   * (Reminder: this should be a pure function that returns a new List !)
+   */
+  def transform(xs: List[Int]): List[Int] = {
+    foldRight(xs, Nil: List[Int])((x, transformed) => Cons(x + 1, transformed))
+  }
+
+  /**
+   * Exercise 3.17
+   * Write a function that turns each value in a List[Double] into a String .
+   * You can use the expression d.toString to convert some d: Double to a String .
+   */
+  def convertToStringList(xs: List[Double]): List[String] = {
+    foldRight(xs, Nil: List[String])((x, converted) => Cons(x.toString, converted))
+  }
+
+  /**
+   * Exercise 3.18
+   * Write a function map that generalizes modifying each element in a list
+   * while maintaining the structure of the list.
+   *
+   * Here is its signature:
+   * def map[A,B](as: List[A])(f: A => B): List[B]
+   */
+  def map[A, B](as: List[A])(f: A => B): List[B] = {
+    foldRight(as, Nil: List[B])((a: A, mapped: List[B]) => Cons(f(a), mapped))
+  }
+
+  /**
+   * Exercise 3.19
+   * Write a function filter that removes elements from a listi
+   * unless they satisfy a given predicate.
+   * Use it to remove all odd numbers from a List[Int].
+   *
+   * def filter[A](as: List[A])(f: A => Boolean): List[A]
+   */
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = {
+    foldRight(as, Nil: List[A])((a, filtered) => a match {
+      case x if f(x) => Cons(x, filtered)
+      case _ => filtered
+    })
+  }
+
+  /**
+   * Exercise 3.20
+   * Write a function flatMap that works like map
+   * except that the function given will return a list instead of a single result,
+   * and that list should be inserted into the final resulting list.
+   *
+   * Here is its signature:
+   * def flatMap[A,B](as: List[A])(f: A => List[B]): List[B]
+   *
+   * For instance, flatMap(List(1,2,3))(i => List(i,i))
+   * should result in List(1,1,2,2,3,3) .
+   */
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = {
+    foldLeft(as, Nil: List[B])((flatMapped, a) => List.append(flatMapped, f(a)))
+  }
+
+  /**
+   * Exercise 3.21
+   * Use flatMap to implement filter.
+   */
+  def filterViaFlatMap[A](as: List[A])(f: A => Boolean): List[A] = {
+    List.flatMap(as)(a => a match {
+      case x if f(x) => List(x)
+      case _ => Nil
+    })
+  }
+
+  /**
+   * Exercise 3.22
+   * Write a function that accepts two lists
+   * and constructs a new list by adding corresponding elements.
+   *
+   * For example, List(1,2,3) and List(4,5,6) become List(5,7,9) .
+   */
+  def addTwoList(as: List[Int], bs: List[Int]): List[Int] = as match {
+    case Nil => bs
+    case Cons(x, xs) =>
+      bs match {
+        case Nil => as
+        case Cons(y, ys) => Cons(x + y, addTwoList(xs, ys))
+      }
+  }
+
+  /**
+   * Exercise 3.23
+   *
+   * Generalize the function you just wrote
+   * so that it's not specific to integers or addition.
+   * Name your generalized function zipWith.
+   */
+  def zipWith[A, B, C](as: List[A], bs: List[B], az: A, bz: B)(f: (A, B) => C): List[C] = as match {
+    case Nil => map(bs)((b) => f(az, b))
+    case Cons(x, xs) =>
+      bs match {
+        case Nil => map(as)((a) => f(a, bz))
+        case Cons(y, ys) => Cons(f(x, y), zipWith(xs, ys, az, bz)(f))
+      }
+  }
+
+  /**
+   * Exercise 3.24 - Hard
+   *
+   * As an example, implement hasSubsequence
+   * for checking whether a List contains another List as a subsequence.
+   * For instance, List(1,2,3,4) would have
+   * List(1,2) , List(2,3) , and List(4) as subsequences, among others.
+   * You may have some difficulty finding a concise purely functional implementation
+   * that is also efficient.
+   * That’s okay. Implement the function however comes most naturally.
+   * We’ll return to this implementation in chapter 5 and hopefully improve on it.
+   *
+   * Note: Any two values x and y can be compared for equality in Scala
+   * using the expression x == y.
+   *
+   * def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean
+   */
 }
--- src/test/scala/fpinscala/exercises/ch03datastructures/ListSuite.scala
+++ src/test/scala/fpinscala/exercises/ch03datastructures/ListSuite.scala
@@ -107,4 +107,163 @@ class ListSuite extends FunSuite {
     val actual = List.concat(listsList)
     assert(actual == expected)
   }
+
+  test("transform a list of integers by adding 1") {
+    val listsList = List(1, 2, 3, 4)
+    val expected = List(2, 3, 4, 5)
+    val actual = List.transform(listsList)
+    assert(actual == expected)
+  }
+
+  test("transform Nil list by adding 1") {
+    val listsList = Nil
+    val expected = Nil
+    val actual = List.transform(listsList)
+    assert(actual == expected)
+  }
+
+  test("convert List of Double to list of String") {
+    val listsList = List(1.0, 2.345, 5.93)
+    val expected = List("1.0", "2.345", "5.93")
+    val actual = List.convertToStringList(listsList)
+    assert(actual == expected)
+  }
+
+  test("convert Nil List of Double to list of String") {
+    val listsList = Nil
+    val expected = Nil
+    val actual = List.convertToStringList(listsList)
+    assert(actual == expected)
+  }
+
+  test("map by adding") {
+    val as = List(1, 2, 3, 4)
+    val expected = List(2, 3, 4, 5)
+    val actual = List.map(as)(_ + 1)
+    assert(actual == expected)
+  }
+
+  test("map by transform") {
+    val as = List(1, 2, 3, 4)
+    val expected = List("1", "2", "3", "4")
+    val actual = List.map(as)(_.toString)
+    assert(actual == expected)
+  }
+
+  test("filter by larger than 2") {
+    val as = List(1, 2, 3, 4)
+    val expected = List(3, 4)
+    val actual = List.filter(as)(_ > 2)
+    assert(actual == expected)
+  }
+
+  test("filter by is not Nil") {
+    val as = List(1, Nil, 3, 4)
+    val expected = List(1, 3, 4)
+    val actual = List.filter(as)(_ != Nil)
+    assert(actual == expected)
+  }
+
+  test("flatMap by duplicate elements") {
+    val as = List(1, 2, 3)
+    val expected = List(1, 1, 2, 2, 3, 3)
+    val actual = List.flatMap(as)(i => List(i, i))
+    assert(actual == expected)
+  }
+
+  test("flatMap to implement filter") {
+    val as = List(1, 2, 3)
+    val expected = List(3)
+    val actual = List.flatMap(as)(i => i match {
+      case x if(x > 2) => List(x)
+      case _ => Nil
+    })
+    assert(actual == expected)
+  }
+
+  test("filterViaFlatMap by larger than 2") {
+    val as = List(1, 2, 3, 4)
+    val expected = List(3, 4)
+    val actual = List.filterViaFlatMap(as)(_ > 2)
+    assert(actual == expected)
+  }
+
+  test("filterViaFlatMap by is not Nil") {
+    val as = List(1, Nil, 3, 4)
+    val expected = List(1, 3, 4)
+    val actual = List.filterViaFlatMap(as)(_ != Nil)
+    assert(actual == expected)
+  }
+
+  test("addTwoList with normal value") {
+    val as = List(1, 2, 3)
+    val bs = List(4, 5, 6)
+    val expected = List(5, 7, 9)
+    val actual = List.addTwoList(as, bs)
+    assert(actual == expected)
+  }
+
+  test("addTwoList with one Nil") {
+    val as = List(1, 2, 3)
+    val bs = List()
+    val expected = List(1, 2, 3)
+    val actual = List.addTwoList(as, bs)
+    assert(actual == expected)
+  }
+
+  test("addTwoList with two Nil") {
+    val as = Nil
+    val bs = List()
+    val expected = Nil
+    val actual = List.addTwoList(as, bs)
+    assert(actual == expected)
+  }
+
+  test("zipWith with normal value") {
+    val as = List(1, 2, 3)
+    val bs = List(4, 5, 6)
+    val expected = List(5, 7, 9)
+    val actual = List.zipWith(as, bs, 0, 0)(_ + _)
+    assert(actual == expected)
+  }
+
+  test("zipWith with one Nil") {
+    val as = List(1, 2, 3)
+    val bs = List()
+    val expected = List(1, 2, 3)
+    val actual = List.zipWith(as, bs, 0, 0)(_ + _)
+    assert(actual == expected)
+  }
+
+  test("zipWith with two Nil") {
+    val as = Nil
+    val bs = List()
+    val expected = Nil
+    val actual = List.zipWith(as, bs, 0, 0)(_ + _)
+    assert(actual == expected)
+  }
+
+  test("zipWith with string and int normal value") {
+    val as = List("1", "2", "3")
+    val bs = List(1, 3, 3)
+    val expected = List(true, false, true)
+    val actual = List.zipWith(as, bs, "", 0)(_ == _.toString)
+    assert(actual == expected)
+  }
+
+  test("zipWith with string and int one Nil") {
+    val as = List("1", "2", "3")
+    val bs = List()
+    val expected = List(false, false, false)
+    val actual = List.zipWith(as, bs, "", 0)(_ == _.toString)
+    assert(actual == expected)
+  }
+
+  test("zipWith with string and int two Nil") {
+    val as = Nil
+    val bs = List()
+    val expected = Nil
+    val actual = List.zipWith(as, bs, "", 0)(_ == _.toString)
+    assert(actual == expected)
+  }
 }
