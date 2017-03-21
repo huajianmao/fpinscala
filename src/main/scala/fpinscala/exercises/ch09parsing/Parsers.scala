cd9a892d2b121c1d5845f09d797d4ad123438bf5
Huajian Mao <huajianmao@gmail.com>
2017-03-21 13:12:41 +0800

    [WIP] Code snippet from Ch09 section 9.1

---
+++ src/main/scala/fpinscala/exercises/ch09parsing/Parsers.scala
@@ -0,0 +1,18 @@
+package fpinscala.exercises.ch09parsing
+
+trait Parsers[ParseError, Parser[+_]] { self =>
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+  def char(c: Char): Parser[Char]
+  def orString(s1: String, s2: String): Parser[String]
+
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] =
+    ParserOps(f(a))
+
+  case class ParserOps[A](p: Parser[A]) {
+    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+  }
+}
---
+++ src/test/scala/fpinscala/exercises/ch09parsing/ParsersSuite.scala
@@ -0,0 +1,6 @@
+package fpinscala.exercises.ch09parsing
+
+import org.scalatest.FunSuite
+
+class ParsersSuite extends FunSuite {
+}
