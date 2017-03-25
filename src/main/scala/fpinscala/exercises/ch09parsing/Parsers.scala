0586cc3476b615ed963a9cac82174807788090cc
Huajian Mao <huajianmao@gmail.com>
2017-03-25 14:49:34 +0800

    Go through the implementation of ParseError and Location

--- src/main/scala/fpinscala/exercises/ch09parsing/Parsers.scala
+++ src/main/scala/fpinscala/exercises/ch09parsing/Parsers.scala
@@ -143,11 +143,44 @@ trait Parsers[Parser[+_]] { self =>
 }
 
 case class Location(input: String, offset: Int = 0) {
-  ???
+  lazy val line = input.slice(0, offset + 1).count(_ == '\n') + 1
+  lazy val col = input.slice(0, offset + 1).lastIndexOf('\n') match {
+    case -1 => offset + 1
+    case lineStart => offset - lineStart
+  }
+
+  def toError(msg: String): ParseError = ParseError(List((this, msg)))
+  def advanceBy(n: Int): Location = copy(offset = offset + n)
+
+  def currentLine: String = {
+    if (input.length > 1) input.lines.drop(line - 1).next
+    else ""
+  }
+  def columnCaret: String = (" " * (col - 1)) + "^"
 }
 
 case class ParseError(stack: List[(Location, String)] = List()) {
-  ???
+  def push(loc: Location, msg: String): ParseError = copy(stack = ((loc, msg) :: stack))
+  def label[A](s: String): ParseError = ParseError(latestLoc.map((_, s)).toList)
+  def latest: Option[(Location, String)] = stack.lastOption
+  def latestLoc: Option[Location] = latest.map(_._1)
+  def formatLoc(l: Location): String = l.line + "." + l.col
+
+  override def toString: String = {
+    if (stack.isEmpty) "no error message"
+    else {
+      val collapsed = collapseStack(stack)
+      val context = collapsed.lastOption.map("\n\n" + _._1.currentLine).getOrElse("") +
+                    collapsed.lastOption.map("\n" + _._1.columnCaret).getOrElse("")
+      collapsed.map { case (loc, msg) => loc.line.toString + "." + loc.col + " " + msg }
+               .mkString("\n") + context
+    }
+  }
+  def collapseStack(s: List[(Location, String)]): List[(Location, String)] = {
+    s.groupBy(_._1)
+     .mapValues(_.map(_._2).mkString("; "))
+     .toList.sortBy(_._1.offset)
+  }
 }
 
 object Parsers {
