ad9c703272cedc10ccf4e28413e88bf49b528ef9
Huajian Mao <huajianmao@gmail.com>
2017-03-25 09:55:06 +0800

    Working on JSON Parser

---
+++ src/main/scala/fpinscala/exercises/ch09parsing/JSON.scala
@@ -0,0 +1,12 @@
+package fpinscala.exercises.ch09parsing
+
+trait JSON
+
+object JSON {
+  case object JNull extends JSON
+  case class JNumber(get: Double) extends JSON
+  case class JString(get: String) extends JSON
+  case class JBool(get: Boolean) extends JSON
+  case class JArray(get: IndexedSeq[JSON]) extends JSON
+  case class JObject(get: Map[String, JSON]) extends JSON
+}
