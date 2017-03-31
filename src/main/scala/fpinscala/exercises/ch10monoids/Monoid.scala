a92449acb223a32685725242e1d2588abf909f40
Huajian Mao <huajianmao@gmail.com>
2017-03-31 08:42:04 +0800

    Add answer for Exercise 10.12

--- src/main/scala/fpinscala/exercises/ch10monoids/Monoid.scala
+++ src/main/scala/fpinscala/exercises/ch10monoids/Monoid.scala
@@ -221,3 +221,59 @@ object Monoid {
     }
   }
 }
+
+trait Foldable[F[_]] {
+  import Monoid._
+  def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B = {
+    foldMap(as)(f.curried)(endoMonoid[B])(z)
+  }
+  def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B = {
+    foldMap(as)(a => (b: B) => f(b, a))(dual(endoMonoid[B]))(z)
+  }
+  def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B = {
+    foldRight(as)(mb.zero)((a, b) => mb.op(f(a), b))
+  }
+  def concatenate[A](as: F[A])(m: Monoid[A]): A = foldLeft(as)(m.zero)(m.op)
+  def toList[A](as: F[A]): List[A] = {
+    foldRight(as)(List[A]())(_::_)
+  }
+}
+
+object ListFoldable extends Foldable[List] {
+  override def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B = {
+    as.foldRight(z)(f)
+  }
+  override def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B = {
+    as.foldLeft(z)(f)
+  }
+  override def foldMap[A, B](as: List[A])(f: A => B)(mb: Monoid[B]): B = {
+    foldRight(as)(mb.zero)((a, b) => mb.op(f(a), b))
+  }
+  override def toList[A](as: List[A]): List[A] = as
+}
+
+object IndexedSeqFoldable extends Foldable[IndexedSeq] {
+  override def foldRight[A, B](as: IndexedSeq[A])(z: B)(f: (A, B) => B): B = {
+    as.foldRight(z)(f)
+  }
+  override def foldLeft[A, B](as: IndexedSeq[A])(z: B)(f: (B, A) => B): B = {
+    as.foldLeft(z)(f)
+  }
+  override def foldMap[A, B](as: IndexedSeq[A])(f: A => B)(mb: Monoid[B]): B = {
+    foldRight(as)(mb.zero)((a, b) => mb.op(f(a), b))
+  }
+}
+
+object StreamFoldable extends Foldable[Stream] {
+  override def foldRight[A, B](as: Stream[A])(z: B)(f: (A, B) => B): B = {
+    as.foldRight(z)(f)
+  }
+  override def foldLeft[A, B](as: Stream[A])(z: B)(f: (B, A) => B): B = {
+    as.foldLeft(z)(f)
+  }
+  override def foldMap[A, B](as: Stream[A])(f: A => B)(mb: Monoid[B]): B = {
+    foldRight(as)(mb.zero)((a, b) => mb.op(f(a), b))
+  }
+}
+
+
