658a6675812ac7fe252ebdf62f1acdc824508b24
Huajian Mao <huajianmao@gmail.com>
2017-04-07 08:29:29 +0800

    Add Mon trait

--- src/main/scala/fpinscala/exercises/ch11monads/Monad.scala
+++ src/main/scala/fpinscala/exercises/ch11monads/Monad.scala
@@ -18,3 +18,12 @@ object Functor {
     def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
   }
 }
+
+trait Mon[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = {
+    flatMap(fa)(a => map(fb)(b => f(a, b)))
+  }
+}
